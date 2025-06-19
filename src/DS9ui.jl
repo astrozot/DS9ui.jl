c6a0ddf6040fc530d8ac37780d14b4d9de0958bb
Marco Lombardi <marco.lombardi@gmail.com>
2025-06-19 14:09:49 +0200

    Compat for v1.10

--- src/DS9ui.jl
+++ src/DS9ui.jl
@@ -16,6 +16,15 @@ include("pickobj.jl")
 
 export ds9select, ds9, ds9close, ds9image, ds9image!, ds9delete, ds9pickobj
 
-public ds9cursor, ds9set, ds9get, ds9wcs, ds9fitprofiles, ds9mask, ds9getregions
+macro public(ex)
+    if VERSION >= v"1.11.0-DEV.469"
+        args = ex isa Symbol ? (ex,) : Base.isexpr(ex, :tuple) ? ex.args : error("something informative")
+        esc(Expr(:public, args...))
+    else
+        nothing
+    end
+end
+
+@public ds9cursor, ds9set, ds9get, ds9wcs, ds9fitprofiles, ds9mask, ds9getregions
 
 end
